17fe7ecba6e91d31243e96b4bfc2a2f34f7d6dea
Yuri Numerov <achi@hush.ai>
2016-07-01 11:34:58 +0200

    improved styling

---
+++ cpyou.js
@@ -0,0 +1,11 @@
+// MOV X Y -> Moves the contents of X to Y
+// ADD     -> Adds A and B and puts the result in R
+// SUB     -> Subtracts A and B and puts the result in R
+// PUT     -> Puts a value in the stack
+// POP     -> Gets a value from the stack
+// CMP     -> Compares A and B
+// JMP     -> Does nothing
+// SHL     -> Shifts bits to the left
+// SHR     -> Shifts bits to the right
+// NOP     -> Does nothing
+
