98f3f5e13f4dd05d812d986e2eaa9c5dd0c463b8
Yuri Numerov <achi@hush.ai>
2016-07-01 14:52:03 +0200

    finishing touches to css

--- cpyou.js
+++ cpyou.js
@@ -1,10 +1,12 @@
+// Instruction set:
+//
 // MOV X Y -> Moves the contents of X to Y
 // ADD     -> Adds A and B and puts the result in R
 // SUB     -> Subtracts A and B and puts the result in R
 // PUT     -> Puts a value in the stack
 // POP     -> Gets a value from the stack
 // CMP     -> Compares A and B
-// JMP     -> Does nothing
+// RPT     -> Repeats some instructions in the history and decreases counter until counter is 0
 // SHL     -> Shifts bits to the left
 // SHR     -> Shifts bits to the right
 // NOP     -> Does nothing
